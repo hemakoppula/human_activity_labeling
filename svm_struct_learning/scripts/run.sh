8217449a7ec7ead0619edbe7a5542d4d80fd4aae
Hema Koppula <hema@hemaHPUbuntu>
2013-07-19 13:11:42 -0400

    corrected the path

--- svm_struct_learning/scripts/run.sh
+++ svm_struct_learning/scripts/run.sh
@@ -7,22 +7,15 @@ w=3
 pid=(0 0 0 0)
 for i in `seq 1 4`
 do
-#modelFile=`ls -lrt fold$i/imodels/model.c4.0.m* | cut -f 3 -d '/'| tail -1`
 
 suffix=c$c.e$e.w$w
 modelFile=model.$suffix
 
 modelFolder=fold$i/models
-#ls -lh fold$i/imodels/$modelFile
 echo "out.$method.$modelFile" >> fold$i/lastout.txt
-#mkdir fold$i/logs
-#mkdir fold$i/models
-#mkdir fold$i/imodels
-#mkdir fold$i/pred
 sh run_svm.sh $c $e $i $modelFile $modelFolder $suffix $w &
 p=$!
 pid[$i]=$p
-#sleep 60
 done 
   
 ps
@@ -34,7 +27,7 @@ wait ${pid[4]}
 echo "processes completed!"
 perl get_avg_pr.pl out.$modelFile > avg_pr.$modelFile
 method=$suffix.$cmethod
-perl ../get_confusion_matrix.pl out.$cmethod.$modelFile $method  > confusionM.$method
+perl get_confusion_matrix.pl out.$cmethod.$modelFile $method  > confusionM.$method
 
 rm runinfo
 echo $HOSTNAME >> runinfo
@@ -59,6 +52,3 @@ echo "" >> runinfo
 echo "" >> runinfo
 cat confusionM.$method >> runinfo
 
-#scp runinfo lion.cs.cornell.edu:~/
-#ssh lion.cs.cornell.edu "cat runinfo | mail -s "$method" hema.swetha@gmail.com"
-#ssh lion.cs.cornell.edu "cat runinfo | mail -s "$method" rg495@cornell.edu"
